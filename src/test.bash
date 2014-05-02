f6e0e4ceec7b42328aa10d674268c9370dd0fb20
Yogesh Patel <yogesh.j.patel@oracle.com>
2014-05-02 14:30:08 -0700

    added exit

--- src/test.bash
+++ src/test.bash
@@ -4,5 +4,13 @@ while test true
 do
   echo -n "Enter your name:"
   read NAME
+
+  # break the loop, if input is empty
+  if test "$NAME" = "" 
+  then
+    echo "Bye..."
+    break
+  fi
+
   echo "Hello $NAME !!"
 done
